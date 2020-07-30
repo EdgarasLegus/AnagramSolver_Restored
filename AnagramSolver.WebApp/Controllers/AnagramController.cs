ae5db6fe09a6a78d3b8141d7551d4d50e159ddca
Edgaras Legus <edgaras.legus@visma.com>
2020-07-30 15:57:29 +0300

    Adding paginated list

--- AnagramSolver.WebApp/Controllers/AnagramController.cs
+++ AnagramSolver.WebApp/Controllers/AnagramController.cs
@@ -2,15 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AnagramSolver.Contracts;
+using AnagramSolver.Interfaces;
+using AnagramSolver.WebApp.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AnagramSolver.WebApp.Controllers
 {
     public class AnagramController : Controller
     {
-        public IActionResult Index()
+        private readonly IWordRepository _repository;
+
+        public AnagramController(IWordRepository repository)
         {
-            return View();
+            _repository = repository;
+        }
+
+        public IActionResult Index(int? pageIndex, int pageSize = 100)
+        {
+            try
+            {
+                var wordList = _repository.GetWords().AsQueryable();
+                return View(PaginatedList<Anagram>.CreateAsync((IQueryable<Anagram>)wordList, pageIndex ?? 1, pageSize));
+            }
+            catch(Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+                return View();
+            }
         }
     }
 }
